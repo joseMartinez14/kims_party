df1c79ea4f47fcc0a54227964f7050b1403eb991
Jose Martinez <josemartinez@Joses-MacBook-Pro.local>
2023-10-14 13:14:27 -0600

    Add photos

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -3,7 +3,7 @@ import { COLORS } from '../../utils/Colors';
 import { TEXTS } from '../../utils/Texts';
 import SportsBarIcon from '@mui/icons-material/SportsBar';
 import { useState } from 'react';
-import { datalist } from '../../utils/Data';
+import { datalist, photolist } from '../../utils/Data';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 import DisplayQuestion from './DisplayQuestion';
@@ -22,6 +22,7 @@ interface GameItem {
 
 const Game = () => {
     const [data, setData] = useState<GameItem[]>(datalist);
+    const [photos, setPhotos] = useState<string[]>(photolist);
 
     const getRandomItem = () => {
         const randomIndex = Math.floor(Math.random() * data.length);
@@ -37,8 +38,19 @@ const Game = () => {
         return temp_ob;
     }
 
+    const getRandomPhotoDelete = () => {
+        console.log(photos)
+        const data_temp = [...photos];
+        const randomIndex = Math.floor(Math.random() * data_temp.length);
+        var temp_ob = data_temp[randomIndex];
+        data_temp.splice(randomIndex,1);
+        setPhotos(data_temp);
+        return temp_ob;
+    }
+
     
     const [item, setItem] = useState<GameItem>(getRandomItem);
+    const [photoItem, setPhotoItem] = useState<string>(getRandomPhotoDelete);
     const [showAnswer, setShowAnswer] = useState<boolean>(false);
     const [answerError, setAnswerError] = useState<boolean>(false);
     const navigate = useNavigate();
@@ -53,9 +65,14 @@ const Game = () => {
         if(item.type == 'Question' && showAnswer == false){
             setAnswerError(true);
         } else {
+            
             setItem(getRandomItemDelete());
+            setPhotoItem(getRandomPhotoDelete());
             setShowAnswer(false);
             setAnswerError(false);
+            if(photos.length <= 1){
+                setPhotos(photolist)
+            }
         }
     }
 
@@ -68,30 +85,18 @@ const Game = () => {
             alignItems={'center'} justifyContent={'center'}
         >{ (data.length > 0)? (
             <Box px={4} >
-                <Paper
-                elevation={20}
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    p: 1,
-                    mt:5,
-                    borderRadius:3,
+                <Typography gutterBottom sx={{
+                    color: COLORS.pruebas,
+                    fontSize: '26px',
+                    fontWeight: '600',
+                    pt: 7,
                     textAlign: 'center',
                 }}>
-                    <Typography gutterBottom sx={{
-                        color: COLORS.pruebas,
-                        fontSize: '26px',
-                        fontWeight: '600',
-                        pt: 1,
-                        textAlign: 'center',
-                    }}>
-                        {item.condition} 
-                    </Typography>
-                </Paper>
+                    {item.condition} 
+                </Typography>
                 <Typography gutterBottom sx={{
                     color: COLORS.neutral500,
-                    fontSize: '26px',
+                    fontSize: '22px',
                     fontWeight: '600',
                     textAlign: 'center',
                     pt: 4,
@@ -102,11 +107,32 @@ const Game = () => {
                 }
 
                 <Box display="flex"
-                    alignItems={'center'} justifyContent={'space-evenly'} pt={9}
+                    alignItems={'center'} justifyContent={'space-evenly'} pt={7}
                 >
-                    <Button variant="contained" onClick={handleNext}> Siguiente<SkipNextIcon /></Button>
-                    <Button variant="contained" onClick={handleExit} sx={{ backgroundColor: COLORS.warningMain }}> Salir    <ExitToAppIcon /></Button>
+                    <Button variant="contained" onClick={handleNext}>Siguiente turno<SkipNextIcon /></Button>
+                    <Button variant="contained" onClick={handleExit} sx={{ backgroundColor: COLORS.warningMain }}> Salir <ExitToAppIcon /></Button>
                 </Box>
+
+                <Box display="flex"
+                    alignItems={'center'} justifyContent={'space-evenly'} pt={2}
+                >
+                    <img src={photoItem} style={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'contain',
+                }} />
+                </Box>
+                <Typography gutterBottom sx={{
+                    color: COLORS.black,
+                    fontSize: '12px',
+                    fontWeight: '400',
+                    textAlign: 'center',
+                    pt: 2,
+                    pb:7
+                }}>
+                    *Imagenes ilustrativas. No tienen nada que ver con el juego
+                </Typography>
+                
             </Box>
         ) : 
         (
--- src/utils/Data.ts
+++ src/utils/Data.ts
@@ -4,37 +4,207 @@ const getRandomItem = () => {
     return datalist[randomIndex];
 }
 
+export const photolist = [
+    "https://special-cards-s3-bucket.s3.us-east-2.amazonaws.com/messageImages/IMG_027BFD841FCA-1.jpeg",
+    "https://special-cards-s3-bucket.s3.us-east-2.amazonaws.com/messageImages/IMG_D6037D2E43F7-1.jpeg",
+    "https://special-cards-s3-bucket.s3.us-east-2.amazonaws.com/messageImages/WhatsApp+Image+2023-10-10+at+16.52.22.jpeg",
+    "https://special-cards-s3-bucket.s3.us-east-2.amazonaws.com/messageImages/WhatsApp+Image+2023-10-12+at+18.17.02.jpeg", //Kim con Manrri
+    "https://special-cards-s3-bucket.s3.us-east-2.amazonaws.com/messageImages/WhatsApp+Image+2023-10-12+at+23.38.50.jpeg", // Lucho y polk foto borrosa y graciosa
+    "https://special-cards-s3-bucket.s3.us-east-2.amazonaws.com/messageImages/WhatsApp+Image+2023-10-14+at+10.20.51.jpeg", // Cata y jhoan
+    "https://special-cards-s3-bucket.s3.us-east-2.amazonaws.com/messageImages/WhatsApp+Image+2023-10-14+at+10.21.49.jpeg", // Cata y jhoan
+    "https://special-cards-s3-bucket.s3.us-east-2.amazonaws.com/messageImages/9F26F02B-74A7-43BD-943D-F1F5387AEFEE.JPG", //Con david
+    "https://special-cards-s3-bucket.s3.us-east-2.amazonaws.com/messageImages/C804737A-BB02-4019-8C40-6AD58453D67C.JPG", //Kim sola copn bloqueador
+    "https://special-cards-s3-bucket.s3.us-east-2.amazonaws.com/messageImages/WhatsApp+Image+2023-10-14+at+12.09.21.jpeg", //Kim señal de bro
+    "https://special-cards-s3-bucket.s3.us-east-2.amazonaws.com/messageImages/WhatsApp+Image+2023-10-14+at+12.50.59+(2).jpeg", //Kim y lily bien tostadas
+    "https://special-cards-s3-bucket.s3.us-east-2.amazonaws.com/messageImages/WhatsApp+Image+2023-10-14+at+12.50.59+(1).jpeg", //Samy bien linda
+    "https://special-cards-s3-bucket.s3.us-east-2.amazonaws.com/messageImages/WhatsApp+Image+2023-10-14+at+12.50.59+(1).jpeg", //Kim y yo 
+    "https://special-cards-s3-bucket.s3.us-east-2.amazonaws.com/messageImages/WhatsApp+Image+2023-10-14+at+12.50.59+(3).jpeg", // Kim en la playa bien sexy
+    "https://special-cards-s3-bucket.s3.us-east-2.amazonaws.com/messageImages/WhatsApp+Image+2023-10-14+at+12.50.59.jpeg", // Con las amigas en el concierto de bad bunny
+]
+
+
 export const datalist = [
     {
-        condition: 'Los que tengan mas tiempo de conocer a Kim',
+        condition: 'Los que tengan más tiempo de conocer a Kim',
         action: 'Toman 2 tragos',
         type: 'Random',
         answer: '',
     },
     {
-        condition: 'Los que no le dijeron feliz cumpleaños a Kim el propio día del cumpleaños',
+        condition: '(Grupal) Los que no le dijeron feliz cumpleaños a Kim el propio día del cumpleaños',
         action: 'Toman 1 shot (manda huevo)',
         type: 'Random',
         answer: '',
     },
     {
-        condition: 'Como se llama la mamá de kim?',
-        action: 'Si contesta correctamente reparte 4 tragos sino se los toma',
+        condition: '(Pregunta individual / Sobre Kim) Como se llama la mamá de kim?',
+        action: 'Si contesta correctamente reparte 4 tragos si no se los toma',
         type: 'Question',
         answer: 'Marcela',
     },
     {
-        condition: 'Como se llama el papá de kim?',
-        action: 'Si contesta correctamente reparte 4 tragos sino se los toma',
+        condition: '(Pregunta individual / Sobre Kim) En que U estudia Kim?',
+        action: 'Si contesta correctamente reparte 4 tragos si no se los toma',
+        type: 'Question',
+        answer: 'U Latina',
+    },
+    {
+        condition: '(Pregunta individual / Sobre Kim) Cuál película ve Kim para dormir después de ver una película de miedo?',
+        action: 'Si contesta correctamente reparte 4 tragos si no se los toma',
+        type: 'Question',
+        answer: 'Shrek',
+    },
+    {
+        condition: '(Pregunta individual / Sobre Kim) En donde trabaja kim?',
+        action: 'Si contesta correctamente reparte 4 tragos si no se los toma',
+        type: 'Question',
+        answer: 'Intel',
+    },
+    {
+        condition: '(Pregunta individual / Sobre Kim) Que está estudiando Kim?',
+        action: 'Si contesta correctamente reparte 4 tragos si no se los toma',
+        type: 'Question',
+        answer: 'Administración de empresas.',
+    },
+    {
+        condition: '(Pregunta individual / Sobre Kim) Como se llama el papá de kim?',
+        action: 'Si contesta correctamente reparte 4 tragos si no se los toma',
         type: 'Question',
         answer: 'Christian',
     },
     {
-        condition: 'A que escuela fue Kim?',
-        action: 'Si contesta correctamente reparte 6 tragos sino se toma 3',
+        condition: '(Pregunta individual / Sobre Kim) A que escuela fue Kim?',
+        action: 'Si contesta correctamente reparte 6 tragos si no se toma 3',
         type: 'Question',
         answer: 'Escuela Moya',
     },
+    {
+        condition: '(Reto) Meta la jupa en la piscina',
+        action: 'Si lo hace reparte 2 shots si no toma 1',
+        type: 'Random',
+        answer: '',
+    },
+    {
+        condition: '(Reto) Haga 10 lagartijas',
+        action: 'Si lo logra reparte 4 tragos si no se toma los 4',
+        type: '',
+        answer: '',
+    },
+    {
+        condition: '(Reto) Deje la persona de la derecha le de una nalgada fuerte',
+        action: 'Si acepta reparte 6 tragos si no se toma 5',
+        type: '',
+        answer: '',
+    },
+    {
+        condition: '(Reto) Fondéese lo que está tomando',
+        action: 'Si lo hace reparte 2 shots si no se toma 1',
+        type: '',
+        answer: '',
+    },
+    {
+        condition: '(Grupal) El/La que mando nudes más recientemente',
+        action: 'Reparte 5 tragos',
+        type: '',
+        answer: '',
+    },
+    {
+        condition: '(Grupal) El/La que haya hecho él sin respeto más recientemente',
+        action: 'Reparte 5 tragos',
+        type: '',
+        answer: '',
+    },
+    {
+        condition: '(Grupal) Los claretianos',
+        action: 'Toman 1 shot',
+        type: '',
+        answer: '',
+    },
+    {
+        condition: 'Los que son exnovios de Kim',
+        action: 'No toman nada. No son bienvenidos, vayanse. HPS',
+        type: '',
+        answer: '',
+    },
+    {
+        condition: '(Grupal) Si les cae mal kim',
+        action: 'Reparte 1 trago si no se toman 3',
+        type: '',
+        answer: '',
+    },
+    {
+        condition: '(Grupal) Los que andan ropa interior roja',
+        action: 'Reparten 3 tragos',
+        type: '',
+        answer: '',
+    },
+    {
+        condition: 'El/La que esté más mamad@',
+        action: 'Toma 5 tragos',
+        type: '',
+        answer: '',
+    },
+    {
+        condition: '(Juego) Cultura chupistica de posiciones sexuales',
+        action: 'EL/La que pierde toma 5 tragos',
+        type: '',
+        answer: '',
+    },
+    {
+        condition: '(Pregunta individual / cultura general) ¿Quién escribió La Odisea?',
+        action: 'Si responde bien, reparte 4 tragos, si no se toma 4',
+        type: 'Question',
+        answer: 'Homero',
+    },
+    {
+        condition: '(Pregunta individual / cultura general) ¿Quién pintó “la última cena”?',
+        action: 'Si responde bien, reparte 4 tragos, si no se toma 4',
+        type: 'Question',
+        answer: 'Leonardo da Vinci',
+    },
+    {
+        condition: '(Pregunta individual / cultura general) ¿Qué año llegó Cristóbal Colón a América?',
+        action: 'Si responde bien, reparte 4 tragos, si no se toma 4',
+        type: 'Question',
+        answer: '1492',
+    },
+    {
+        condition: '(Pregunta individual / cultura general) ',
+        action: 'Si responde bien, reparte 4 tragos, si no se toma 4',
+        type: 'Question',
+        answer: '',
+    },
+    {
+        condition: '(Pregunta individual / cultura general) ¿Quién es el padre del psicoanálisis?',
+        action: 'Si responde bien, reparte 4 tragos, si no se toma 4',
+        type: 'Question',
+        answer: 'Sigmund Freud',
+    },
+    {
+        condition: '(Pregunta individual / cultura general) ¿Cuál es el disco más vendido de la historia?',
+        action: 'Si responde bien, reparte 4 tragos, si no se toma 4',
+        type: 'Question',
+        answer: 'Thriller, de Michael Jackson. (Viva el hehe)',
+    },
+    {
+        condition: '(Pregunta individual / cultura general) ¿En qué lugar del cuerpo se produce la insulina?',
+        action: 'Si responde bien, reparte 4 tragos, si no se toma 4',
+        type: 'Question',
+        answer: 'En el páncreas es donde se produce la insulina.',
+    },
+    {
+        condition: '(Pregunta individual / cultura general) ¿Con qué se fabricaba el pergamino?',
+        action: 'Si responde bien, reparte 4 tragos, si no se toma 4',
+        type: 'Question',
+        answer: 'El pergamino se fabrica con los piel de animales',
+    },
+    {
+        condition: '(Pregunta individual / cultura general) ¿Dónde está la Casa Blanca?',
+        action: 'Si responde bien, reparte 4 tragos, si no se toma 4',
+        type: 'Question',
+        answer: 'Washington D.C.',
+    },
+
 ]
 
 
