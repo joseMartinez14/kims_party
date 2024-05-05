a6c81db588469ec21f7a7eb75e037001ef3e8c33
Jose Martinez <josemartinez@Joses-MacBook-Pro.local>
2024-05-05 08:56:41 -0600

    Home buttom

--- src/layouts/MainLayout/Header/index.tsx
+++ src/layouts/MainLayout/Header/index.tsx
@@ -4,14 +4,22 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, makeStyles } from '@mui/material';
 import LiquorIcon from '@mui/icons-material/Liquor';
+import HomeIcon from '@mui/icons-material/Home';
+
 
 import { COLORS } from '../../../utils/Colors';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+
+    const navigate = useNavigate();
 
     return (
         <AppBar position="static" sx={{ backgroundColor: COLORS.darkblue, mb: 2 }}>
             <Toolbar>
+                <div onClick={() => { navigate('/'); }}>
+                    <HomeIcon />
+                </div>
                 <Typography variant="h6" sx={{ flexGrow: 1, textAlign: 'center' }}>
                     Que pario mamá!!
                 </Typography>
